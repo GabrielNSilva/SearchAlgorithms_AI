4de4e2355954b012640a20188ecebfa7e4a710b2
GabrielNSilva <gabrielnsilva1@hotmail.com>
2018-11-26 17:07:39 -0200

    Fator de ramificação efetivo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from Heuristicas import *
 
 from util import *
 
-print('Para ativar ou destivar os prints de debug utilize a flag PRINT_ATIVO no arquivo \'util.py\'')
+print('Para ativar ou destivar os prints de debug utilize a flag PRINT_ATIVO no arquivo \'util.py\'\n')
 printif('Prints de debug ativados......')
 
 # Definindo no objetivo
@@ -15,10 +15,12 @@ obj = Puzzle(3, 3, [[1,2,3],[4,5,6],[7,8,None]])
 objetivo = Node(obj)
 
 # gerando nos aleatórios da profundidade 2 até 8
+quantidade = 10
+print('Gerando estados aleatorios......')
 allstates = list()
 for profundidade in range(2,9):
     # Dez estados de cada profundidade
-    for k in range(10):
+    for k in range(quantidade):
         # Definindo estado inicial
         raiz = Puzzle(3, 3, [[1, 2, 3], [4, 5, 6], [7, 8, None]])
         no_raiz = Node(raiz)
@@ -50,6 +52,7 @@ medias = {
     'ae_h2': [{'stored': 0, 'max_stored': 0, 'iterations': 0, 'time': time_0} for q in range(7)],
 }
 
+print('Realizando buscas......\n')
 for no in allstates:
 
     # armazenando profundidade para calculo do b*
@@ -61,26 +64,32 @@ for no in allstates:
 
     gulosa = BuscaGulosa(no, objetivo, h1)
     for key in medias['gu_h1'][pr]:
-        if pr == 6:
-            print('medias[\'gu_h1\'][',pr,'][',key,'] += ',gulosa[key],'/10')
-        medias['gu_h1'][pr][key] += gulosa[key]/10 # divide por 10 para dar a media, pois sao 10 valores de cada profundidade (pr)
+        medias['gu_h1'][pr][key] += gulosa[key]/quantidade # divide por quantidade para dar a media, pois sao quantidade valores de cada profundidade (pr)
+        # if pr == 6 and key == 'max_stored':
+            # print('medias[\'gu_h1\'][',pr,'][',key,'] += ',gulosa[key],'/quantidade  ->  ',medias['gu_h1'][pr][key])
     # print_reponse(gulosa)
 
     gulosa = BuscaGulosa(no, objetivo, h2)
     for key in medias['gu_h2'][pr]:
-        medias['gu_h2'][pr][key] += gulosa[key]/10 # divide por 10 para dar a media, pois sao 10 valores de cada profundidade (pr)
+        medias['gu_h2'][pr][key] += gulosa[key]/quantidade # divide por quantidade para dar a media, pois sao quantidade valores de cada profundidade (pr)
+        # if pr == 6 and key == 'max_stored':
+            # print('medias[\'gu_h2\'][',pr,'][',key,'] += ',gulosa[key],'/quantidade  ->  ',medias['gu_h2'][pr][key])
     # print_reponse(gulosa)
 
     ######### BuscaAE #########
 
     estrela = BuscaAEstrela(no, objetivo, h1)
     for key in medias['ae_h1'][pr]:
-        medias['ae_h1'][pr][key] += estrela[key]/10 # divide por 10 para dar a media, pois sao 10 valores de cada profundidade (pr)
+        medias['ae_h1'][pr][key] += estrela[key]/quantidade # divide por quantidade para dar a media, pois sao quantidade valores de cada profundidade (pr)
+        # if pr == 6 and key == 'max_stored':
+            # print('medias[\'ae_h1\'][',pr,'][',key,'] += ',estrela[key],'/quantidade  ->  ',medias['ae_h1'][pr][key])
     # print_reponse(estrela)
 
     estrela = BuscaAEstrela(no, objetivo, h2)
     for key in medias['ae_h2'][pr]:
-        medias['ae_h2'][pr][key] += estrela[key]/10 # divide por 10 para dar a media, pois sao 10 valores de cada profundidade (pr)
+        medias['ae_h2'][pr][key] += estrela[key]/quantidade # divide por quantidade para dar a media, pois sao quantidade valores de cada profundidade (pr)
+        # if pr == 6 and key == 'max_stored':
+            # print('medias[\'ae_h2\'][',pr,'][',key,'] += ',estrela[key],'/quantidade  ->  ',medias['ae_h2'][pr][key])
     # print_reponse(estrela)
 
 print_avg_table(medias)
--- util.py
+++ util.py
@@ -42,14 +42,62 @@ def print_avg(response):
 
 
 def print_avg_table(response):
-    print(response)
-
-    print('\t\t Custo de Busca')
-    print(' d | Gulosa (h1) | Gulosa (h2) | A* (h1) | A* (h2) |')
+    printif(response)
     for i in range(7):
-        print(' {0:1d} | {1:5.3f} | {2:5.3f} | {3:5.3f} | {4:5.3f} |'.format(
+        printif('[\'gu_h1\']['+str(i)+'] = '+str(response['gu_h1'][i]))
+        printif('[\'gu_h2\']['+str(i)+'] = '+str(response['gu_h2'][i]))
+        printif('[\'ae_h1\']['+str(i)+'] = '+str(response['ae_h1'][i]))
+        printif('[\'ae_h2\']['+str(i)+'] = '+str(response['ae_h2'][i]))
+
+    print('-------------------Custo de Busca--------------------')
+    print('| d | Gulosa (h1) | Gulosa (h2) | A* (h1) | A* (h2) |')
+    for i in range(7):
+        print('| {0:1d} |    {1:5.1f}    |    {2:5.1f}    |  {3:5.1f}  |  {4:5.1f}  |'.format(
             i+2,
             response['gu_h1'][i]['max_stored'],
             response['gu_h2'][i]['max_stored'],
             response['ae_h1'][i]['max_stored'],
             response['ae_h2'][i]['max_stored']))
+
+    print('------------Fator de Ramificação Efetivo-------------')
+    print('| d | Gulosa (h1) | Gulosa (h2) | A* (h1) | A* (h2) |')
+    for i in range(7):
+        # Cálculo do fator de ramificacao (b*)
+        response['gu_h1'][i]['b*'] = fator_ramificacao(response['gu_h1'][i]['max_stored'], i+2)
+        response['gu_h2'][i]['b*'] = fator_ramificacao(response['gu_h2'][i]['max_stored'], i+2)
+        response['ae_h1'][i]['b*'] = fator_ramificacao(response['ae_h1'][i]['max_stored'], i+2)
+        response['ae_h2'][i]['b*'] = fator_ramificacao(response['ae_h2'][i]['max_stored'], i+2)
+
+        print('| {0:1d} |    {1:5.3f}    |    {2:5.3f}    |  {3:5.3f}  |  {4:5.3f}  |'.format(
+            i+2,
+            response['gu_h1'][i]['b*'],
+            response['gu_h2'][i]['b*'],
+            response['ae_h1'][i]['b*'],
+            response['ae_h2'][i]['b*']))
+    print('-----------------------------------------------------')
+
+def fator_ramificacao(N, d):
+    printif('*********************************************** fator_ramificacao('+str(N)+', '+str(d)+')')
+    hv = 10
+    mv = 0
+    b = (hv+mv)/2
+    printif('hv'+str(hv)+' mv'+str(mv)+' -> b'+str(b))
+    nbd = n(b, d)
+    printif(str(N)+'-'+str(nbd)+'='+str(N - nbd))
+    while abs(N - nbd) > 0.001:
+        if N < nbd:
+            hv = b
+        else:
+            mv = b
+        b = (hv+mv)/2
+        printif('hv '+str(hv)+'  mv '+str(mv)+'  -> b '+str(b))
+        nbd = n(b, d)
+        printif(str(N)+' - '+str(nbd)+' = '+str(N - nbd))
+        # sleep(0.5)
+    return b
+
+def n(b, d):
+    x = 0
+    for de in range(d):
+        x += b**de
+    return x
